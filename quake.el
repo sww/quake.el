448ac045daf4a092d285489454b13362c5897854
Sean Wang <sean@decrypted.org>
2018-12-20 20:23:16 -0800

    Refactored to make things a little more readable.
    * Removed a nested let.
    * Set the local variables within the body.

--- quake.el
+++ quake.el
@@ -52,22 +52,24 @@
 (defun quake ()
   "Provides Quake-like buffer splits."
   (interactive)
-  (let ((height-or-width (if (member quake-window-position '(below above))
-                             (frame-height)
-                           (frame-width))))
-    (let ((quake-split-size (- height-or-width (floor (* height-or-width quake-max-window-size)))))
-      (if (get-buffer-window quake-buffer-name)
-          ;; Probably already ran the function, so hide the frame.
-          (delete-window (get-buffer-window quake-buffer-name))
+  (let (height-or-width quake-split-size)
+    (setq height-or-width (if (member quake-window-position '(below above))
+                              (frame-height)
+                            (frame-width)))
+    (setq quake-split-size (- height-or-width (floor (* height-or-width quake-max-window-size))))
 
-        ;; Create the window split.
-        (select-window (split-window (frame-root-window) quake-split-size))
+    (if (get-buffer-window quake-buffer-name)
+        ;; Probably already ran the function, so hide the frame.
+        (delete-window (get-buffer-window quake-buffer-name))
 
-        ;; Check if a shell buffer exists already, or create one.
-        (if (get-buffer quake-buffer-name)
-            (switch-to-buffer (get-buffer quake-buffer-name))
-          (funcall quake-term-function quake-term-args)
-          (rename-buffer quake-buffer-name))))))
+      ;; Create the window split.
+      (select-window (split-window (frame-root-window) quake-split-size))
+
+      ;; Check if a shell buffer exists already, or create one.
+      (if (get-buffer quake-buffer-name)
+          (switch-to-buffer (get-buffer quake-buffer-name))
+        (funcall quake-term-function quake-term-args)
+        (rename-buffer quake-buffer-name)))))
 
 (provide 'quake)
 ;;; quake.el ends here
